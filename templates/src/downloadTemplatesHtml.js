93f354c50bfcf0c7188f0a30ee3421251e8a7b32
ThanhLM <thanhlm@bravebits.vn>
2022-03-18 09:41:30 +0700

    extract css to inline style instead of using cdn

--- templates/src/downloadTemplatesHtml.js
+++ templates/src/downloadTemplatesHtml.js
@@ -26,57 +26,62 @@ module.exports.downloadTemplatesHtml = async () => {
         }
         for (let i = 0; i < templateHandles.length; i++) {
             const template = templateHandles[i];
-
-            axios.get(`${PREFIX}/${template}`).then((res) => {
-                const dom = new jsdom.JSDOM(res.data);
-                dom.window.document.querySelector(".pf-btn-install").remove();
-
-                // remove scripts
-                const allScripts = dom.window.document.querySelectorAll("script");
-                allScripts.forEach((script) => {
-                    if (
-                        (script.src && !script.src?.includes("pagefly")) ||
-                        (!script.src && !script.innerHTML?.includes("pagefly")) ||
-                        excludeScripts.some((str) => script?.src?.includes(str)) ||
-                        excludeScripts.some((str) => script?.innerHTML?.includes(str))
-                    ) {
-                        script.remove();
-                    }
-                });
-
-                // remove all unwanted link tags
-                let allLinkTags = dom.window.document.querySelectorAll("link");
-                allLinkTags.forEach((link) => {
-                    if (excludeLinks.some((str) => link?.href?.includes(str))) {
-                        link.remove();
-                    }
-                });
-
-                // update some link href
-                allLinkTags = dom.window.document.querySelectorAll("link");
-                allLinkTags.forEach((link) => {
-                    if(link.href.startsWith('//cdn')) {
-                        link.href = "https:" + link.href;
-                    }
-                });
-                let imgTags = dom.window.document.querySelectorAll('img');
-                imgTags.forEach((img) => {
-                    if(img.src.startsWith('//cdn')) {
-                        img.src = "https:" + img.src;
-                    }
-                })
-
-                // remove all unwanted tags
-                excludeTags.forEach((tag) => {
-                    const allTags = dom.window.document.querySelectorAll(tag);
-                    allTags.forEach((tag) => {
-                        tag.remove();
-                    });
-                });
-
-                fs.writeFileSync(`html/${template}.html`, dom.serialize());
-                console.log(`Downloaded ${template}`);
+            const res = await axios.get(`${PREFIX}/${template}`)
+            const dataRemovedComment = res.data.replace(/<!--[\s\S]*?-->/g, "");
+            const dom = new jsdom.JSDOM(dataRemovedComment);
+            dom.window.document.querySelector(".pf-btn-install").remove();
+            // remove scripts
+            const allScripts = dom.window.document.querySelectorAll("script");
+            allScripts.forEach((script) => {
+                if (
+                    (script.src && !script.src?.includes("pagefly")) ||
+                    (!script.src && !script.innerHTML?.includes("pagefly")) ||
+                    excludeScripts.some((str) => script?.src?.includes(str)) ||
+                    excludeScripts.some((str) => script?.innerHTML?.includes(str))
+                ) {
+                    script.remove();
+                }
             });
+
+            // remove all unwanted link tags
+            let allLinkTags = dom.window.document.querySelectorAll("link");
+            allLinkTags.forEach((link) => {
+                if (excludeLinks.some((str) => link?.href?.includes(str))) {
+                    link.remove();
+                }
+            });
+
+            // update some link href
+            allLinkTags = dom.window.document.querySelectorAll("link");
+            allLinkTags.forEach((link) => {
+                if(link.href.startsWith('//cdn')) {
+                    link.href = "https:" + link.href;
+                }
+            });
+            let imgTags = dom.window.document.querySelectorAll('img');
+            imgTags.forEach((img) => {
+                if(img.src.startsWith('//cdn')) {
+                    img.src = "https:" + img.src;
+                }
+            })
+
+            // remove all unwanted tags
+            excludeTags.forEach((tag) => {
+                const allTags = dom.window.document.querySelectorAll(tag);
+                allTags.forEach((tag) => {
+                    tag.remove();
+                });
+            });
+
+            const cssLinks = dom.window.document.querySelectorAll('link')
+            const styleData = await Promise.all(Array.from(cssLinks).map(async link => {
+                return await axios.get(link.href).then(res => res.data)
+            }))
+            const css = `<style>${styleData.join('')}</style>`
+            dom.window.document.querySelector('html').innerHTML = css + dom.window.document.querySelector('html').innerHTML
+
+            fs.writeFileSync(`html/${template}.html`, dom.serialize());
+            console.log(`Downloaded ${template}`);
         }
     } catch (e) {
         console.log(e);
